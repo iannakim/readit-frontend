932dcd7e02541980f8bcce8c120e7cdf2a98ab06
iannakim <dear.hyunji@gmail.com>
2021-02-25 17:05:35 -0500

    Bg images changed

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,29 +4,10 @@ import './home.css'
 import bg from '../images/bg.jpg';
 import cloud1 from '../images/cloud1.png';
 import cloud2 from '../images/cloud2.png';
-import ironman from '../images/ironman.png';
-import jet from '../images/jet.png';
-import moon from '../images/moon.png';
+import quote from '../images/quote.png';
+import spaceship from '../images/spaceship.png';
 
 class Home extends React.Component {
-
-  componentDidMount (){
-    const script1 = document.createElement("script")
-    const script2 = document.createElement("script")
-
-    script1.src="https://cdnjs.cloudflare.com/ajax/libs/gsap/3.6.0/gsap.min.js";
-    script1.async = true;
-
-    script2.src="https://cdnjs.cloudflare.com/ajax/libs/gsap/3.6.0/ScrollTrigger.min.js";
-    script2.async = true;
-
-    document.body.appendChild(script1)
-    document.body.appendChild(script2)
-
-  }
-
-
-
 
 
   render(){
@@ -34,11 +15,10 @@ class Home extends React.Component {
       <div className="Home">
         <section>
           <img src={bg} id="bg"/>
-          <img src={moon} id="moon"/>
           <img src={cloud1} id="cloud1"/>
+          <img src={spaceship} id="spaceship"/>
           <img src={cloud2} id="cloud2"/>
-          <img src={jet} id="jet"/>
-          <img src={ironman} id="ironman"/>
+          <img src={quote} id="quote"/>
         </section>
         <div className="sec">
           <div className="content">
